5e1ee6c1823255052ad2e43e9fbe6a8965a5037f
Josh Kroslowitz <joshkroz@gmail.com>
2022-02-06 02:17:44 +0800

    Media NFT passed tests

--- contracts/Media.sol
+++ contracts/Media.sol
@@ -1,16 +1,17 @@
 pragma solidity ^0.8.10;
 
 import "@openzeppelin/contract/token/ERC721/ERC721.sol";
+import "@openzeppelin/contract/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract Media is ERC721 {
+contract Media is ERC721URIStorage {
 
 	uint256 public tokenCounter;
 
-	constructor () public ERC721 ("Meeda", "MEED") {
+	constructor () ERC721 ("Meeda", "MEED") {
 		tokenCounter = 0;
 	}
 
-	function createMedia(string memory tokenURI) public return (uint256) {
+	function createMedia(string memory tokenURI) public returns (uint256) {
 		uint256 newItemId = tokenCounter;
 		_safeMint(msg.sender, newItemId);
 		_setTokenURI(newItemId, tokenURI);
