f379d9dfb977e0ff5961e73b87e4db5fdc5a8ec8
gourav <gvirus21yt@gmail.com>
2022-02-06 19:15:27 +0530

    updated subscribe button

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -20,11 +20,18 @@ const userName = "John Doe";
 
 const Profile = () => {
   const [images, setImages] = useState([]);
-  const {account} = useContext(UserContext);
+  const [subscribeText, setSubscribeText] = useState("subscribe");
+  const { account } = useContext(UserContext);
 
   let userAddress;
-  let profilePic;
 
+  const handleSubscribe = () => {
+    if (subscribeText === "subscribe") {
+      setSubscribeText("unsubscribe");
+    } else if (subscribeText === "unsubscribe") {
+      setSubscribeText("subscribe");
+    }
+  };
   useEffect(() => {
     Axios.get(
       "https://api.unsplash.com/photos/?client_id=l2U-D_PXXujBJoRiCCMCL2ifi_5ZJcK4AC0WH-A2lKk"
@@ -44,20 +51,12 @@ const Profile = () => {
             {/* <ProfileImage src={}/> */}
           </ProfileImageContainer>
           <ProfileInfoContainer>
-
             <ProfileName>{userName}</ProfileName>
-
             <ProfileAddress>{account}</ProfileAddress>
-            <SubscribeButton onClick={console.log("")} />
-
-            <ProfileAddress>{userAddress}</ProfileAddress>
-            <SubscribeButton onClick={console.log("clicked")}>
-
-              Subscribe
+            <SubscribeButton onClick={handleSubscribe}>
+              {subscribeText}
             </SubscribeButton>
-
           </ProfileInfoContainer>
-
         </ProfileInnerContainer>
       </ProfileTopContainer>
       <MainGridImageContainer>
